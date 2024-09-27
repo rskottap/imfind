3057112715a0b02cb4f6ba0be33bc966ea9a7b20
Ramya Kottapalli <rskottap@gmail.com>
2024-09-26 17:17:21 -0700

    Fix localvariable error in script.

--- src/imfind/bin/imfind.py
+++ src/imfind/bin/imfind.py
@@ -5,18 +5,18 @@ __all__ = ['main']
 import os
 import sys
 import argparse
-from .config import default_prompt, file_types
+from . import config
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    parser = argparse.ArgumentParser('Find image')
+    parser = argparse.ArgumentParser('imfind')
     parser.add_argument('description', type=str, help="Description of the image to find.")
     parser.add_argument('-d', '--directory', type=str, help="Directory to search in. Defaults to $HOME.")
     parser.add_argument('-n', '--threshold', type=int, default=10, help="Top n results to output. Defaults to top 10. Use -1 to list all images sorted by relevance.")
     parser.add_argument('-p', '--prompt', type=str, help="Additional user prompt that gets appended to default prompt. Used to generate description of images.")
-    parser.add_argument('-t', '--types', type=str, nargs='*', default=file_types, help=f"Additional image types to search. By default searches for images with  extensions {file_types}")
+    parser.add_argument('-t', '--types', type=str, nargs='*', default=config.file_types, help=f"Additional image types to search. By default searches for images with  extensions {config.file_types}")
     parser.add_argument('--include-hidden', action='store_true', help="Include hidden directories like ones starting with '.' or '__'. For example, within .cache or __pycache__ etc., Excludes these by default.")
     args = parser.parse_args(argv)
     
@@ -36,9 +36,9 @@ def main(argv=None):
         print("Make sure $HOME is set or provide an existing directory to search within.")
         raise e
     
-    file_types = list(set(file_types + (args.types or [])))
+    file_types = list(set(config.file_types + (args.types or [])))
     thres = args.threshold
-    final_prompt = default_prompt + (args.prompt or '').strip()
+    final_prompt = config.default_prompt + (args.prompt or '').strip()
     include_hidden = bool(args.include_hidden)
 
     # do the image search
