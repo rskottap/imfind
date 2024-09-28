c831e7ddb54928ce0fbe71def1455ed4b1ff3356
Ramya Kottapalli <rskottap@gmail.com>
2024-09-28 09:58:55 -0700

    Add threshold output logic.

--- src/imfind/bin/imfind.py
+++ src/imfind/bin/imfind.py
@@ -7,20 +7,31 @@ import sys
 import argparse
 from . import config
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
+def parse_args(argv):
+
+    def check_nonneg(value):
+        ivalue = int(value)
+        if ivalue < 0:
+            raise argparse.ArgumentTypeError(f"Threshold needs to be an integer greater than or equal to 1 (n>=1).\n Passed {value}.")
+        return ivalue
 
     parser = argparse.ArgumentParser('imfind')
     parser.add_argument('description', type=str, help="Description of the image to find.")
     parser.add_argument('-d', '--directory', type=str, help="Directory to search in. Defaults to $HOME.")
-    parser.add_argument('-n', '--threshold', type=int, default=10, help="Top n results to output. Defaults to top 10. Use -1 to list all images sorted by relevance.")
+    parser.add_argument('-n', '--threshold', type=check_nonneg, default=0, help="Top n (>=1) results to output. By default lists all images found (like the find command) sorted from most relevant/similar to least.")
     parser.add_argument('-p', '--prompt', type=str, help="Additional user prompt that gets appended to default prompt. Used to generate description of images.")
     parser.add_argument('-t', '--types', type=str, nargs='*', default=config.file_types, help=f"Additional image types to search. By default searches for images with  extensions {config.file_types}")
     parser.add_argument('--no-cache', action='store_true', help="Do not read from existing cache. Overwrites cache with new model generations. (Internally sets and uses USE_MMRY_CACHE environment variable).")
     parser.add_argument('--include-hidden', action='store_true', help="Include hidden directories like ones starting with '.' or '__'. For example, within .cache or __pycache__ etc., Excludes these by default.")
     args = parser.parse_args(argv)
-    
+    return args
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    args = parse_args(argv)
+
     description = args.description.strip()
     if not description:
         raise Exception("Error: Empty description. Please provided a detailed description of the image to find.")
@@ -52,10 +63,14 @@ def main(argv=None):
 
     top = image_search(user_img_desc=description, gen_desc_prompt=final_prompt, 
                        directory=directory, file_types=file_types, include_hidden=include_hidden)
-    print('\n'.join(top))
 
     # delete variable
     __ = os.environ.pop("USE_MMRY_CACHE")
+
+    if thres:
+        print('\n'.join(top[:thres]))
+    else:
+        print('\n'.join(top))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
